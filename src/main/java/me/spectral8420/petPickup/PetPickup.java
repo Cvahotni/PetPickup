43c13949eefd1e026f38d2fde87d222d25b82502
cvahotni <libraryplaceforcode42@tutanota.com>
2026-05-23 19:04:13 +1000

    Fix typos, add startup and shutdown messages, tested for 26.1.2

--- src/main/java/me/spectral8420/petPickup/PetPickup.java
+++ src/main/java/me/spectral8420/petPickup/PetPickup.java
@@ -7,6 +7,8 @@ import me.spectral8420.petPickup.config.CustomConfigManager;
 import me.spectral8420.petPickup.event.MainListener;
 import me.spectral8420.petPickup.misc.Lang;
 import me.spectral8420.petPickup.misc.Settings;
+import me.spectral8420.petPickup.util.ConsoleHelper;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PetPickup extends JavaPlugin {
@@ -25,10 +27,12 @@ public final class PetPickup extends JavaPlugin {
         }
 
         getServer().getPluginManager().registerEvents(new MainListener(this), this);
+        ConsoleHelper.sendMessage(ChatColor.GREEN + "PetPickup has been enabled!");
     }
 
     @Override
     public void onDisable() {
         CustomConfigManager.save();
+        ConsoleHelper.sendMessage(ChatColor.RED + "PetPickup has been disabled!");
     }
 }
